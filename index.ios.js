eb5698cb9d6aef5aa667792c268c0c2c5bafa047
Furkan Çelik <furkan@Furkan-MacBook-Pro.local>
2016-10-21 04:34:34 +0300

    Dosya içeriği düzenlendi

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,31 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
 } from 'react-native';
 
-export default class RNACS extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+var ListPage = require('./App/Components/ListPage.js');
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+import { List } from './App/Store/List.js';
+import { Configs } from './App/config.js';
+
+
+// mainProps Değişkeni ile Configs, List vs gibi tüm storeleri
+// alt Componentlere aktarımını daha rahat sağlar.
+
+// var mainProps = {
+//   list:List,
+//   ...Configs,
+// };
+
+export default class RNACS extends Component {
+
+  render() {
+    return(<ListPage list={List} />)
+  }
+
+}
 
 AppRegistry.registerComponent('RNACS', () => RNACS);
