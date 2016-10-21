69902e3231d385ca7d3346ab6e5acbe14b021bf9
Furkan Çelik <furkan@Furkan-MacBook-Pro.local>
2016-10-21 03:38:19 +0300

    Uygulamanın Config Dosyası Oluşturuldu

---
+++ App/config.js
@@ -0,0 +1,10 @@
+  //
+  // Uygulama İçin Sabit Değerler
+  // URL , Başlıklar vs gibi sabit değerler
+  // Uygulamanın heryerinde ulaşılabilecek değerler.
+  //
+export const Configs = {
+  //Fetch vs Api İçin URL Adresi
+  URL:"http://localhost/",
+};
+
